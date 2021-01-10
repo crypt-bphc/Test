342327ce616edd24b4e808cd84580df2f45af3d8
crypt-bphc <76141508+crypt-bphc@users.noreply.github.com>
2021-01-10 10:08:37 +0530

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -23,8 +23,7 @@ sudo adduser Arihant chrome-remote-desktop
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
-CRP = "${{ secrets.URL_CONFIG }}"
-su - Arihant -c """$CRP"""
+su - Arihant -c """${{ secrets.URL_CONFIG }}"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
